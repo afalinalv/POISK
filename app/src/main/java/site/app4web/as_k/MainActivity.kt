bef6f868038e117a509287e31f0a80727fce3bdd
afalinalv <del.afalina@gmail.com>
2019-04-11 07:17:42 +0300

    Ver 1.2 Start New Ping Готово под TEST Код приводить в порядок

--- app/src/main/java/site/app4web/as_k/MainActivity.kt
+++ app/src/main/java/site/app4web/as_k/MainActivity.kt
@@ -1,38 +1,51 @@
 package site.app4web.as_k
 
+import android.annotation.TargetApi
 import android.content.Context
 import android.content.Intent
+
+
 import android.net.ConnectivityManager
 import android.net.Uri
+import android.os.AsyncTask
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.StrictMode
 
 import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.*
 import com.google.api.client.http.javanet.NetHttpTransport
 import com.google.api.client.json.jackson2.JacksonFactory
 import com.google.api.services.customsearch.Customsearch
 import com.google.api.services.customsearch.CustomsearchRequestInitializer
+import com.google.api.services.customsearch.model.Search
 
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.IOException
 import java.net.HttpURLConnection
 import java.net.MalformedURLException
 import java.net.URL
+import java.util.concurrent.ExecutionException
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
 
 class MainActivity : AppCompatActivity() {
     val myCX = "003159383193150926956:c6m9ah2oy8e" //Your search engine
     val myKey = "AIzaSyCL_iY2ALQxC4w6Qfld253adeI_GGl_bmw"
     val myApp = "APP4WEB"
     lateinit var cs : Customsearch
+    internal var httpEdit: String = ""
+    private var pingHttpTask: PingHttpTask? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder().permitAll().build())
+        //StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder().permitAll().build())
 
         if (isConnected())
             Toast.makeText(applicationContext, "Инет Есть", Toast.LENGTH_SHORT).show()
@@ -49,68 +62,189 @@ class MainActivity : AppCompatActivity() {
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE)  as InputMethodManager
         imm.hideSoftInputFromWindow(button.windowToken,InputMethodManager.HIDE_NOT_ALWAYS)
 
-        var TryText = editText.text.toString()
-        if (tryHttp(TryText)) { // сайт пингуется
+         httpEdit = editText.text.toString()
+        val Zapros = listTryHttp(httpEdit)
+        if (tryHttp1(Zapros)) { // сайт пингуется
+            editText.setText(httpEdit)
             Toast.makeText(applicationContext, "Сайт доступен", Toast.LENGTH_SHORT).show()
-            TryText = editText.text.toString()
-            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(TryText)))
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(httpEdit)))
             return
         } else {   // сайт НЕ пингуется
             Toast.makeText(applicationContext, "Сайт НЕ доступен", Toast.LENGTH_SHORT).show()
         }
+
+        if (tryHttp2(Zapros)) { // работает изучать
+            editText.setText(httpEdit)
+            //textView.setText("!!!!!!!!Сайт доступен   $httpEdit")
+            Toast.makeText(applicationContext, "!!!!!!!!Сайт доступен  $httpEdit", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(httpEdit)))
+        } else {
+           // textView.setText("$httpEdit  Сайт НЕ доступен#######")
+            Toast.makeText(applicationContext, "$httpEdit Сайт НЕ доступен#######", Toast.LENGTH_SHORT).show()
+           // webView.loadUrl("")
+
+        }
+        // Вызов GOOGLE Search
         val searchQuery = editText.text.toString()  //The query to search
-        Poisk(searchQuery)
+        val poisk = Poisk().execute (searchQuery)
+        val result = poisk.get()
+        val LINK = LV(result)
+     //   if (LINK != null) httpEdit = LINK
     }
     fun isConnected(): Boolean {
         val ni = (getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager)
             .activeNetworkInfo
         return ni != null && ni.isConnected
     }
-    fun tryHttp(url: String): Boolean {
-        var url=url
-        // Если точно не САЙТ - в ПОИСК
-        if (!url.contains("."))     return false
-        url = url.trim { it <= ' ' }
-        if (url.startsWith("."))    return false
-        if (url.endsWith("."))      return false
-        if (url.contains(" "))      return false
-        // Если WWW или м.б. сайт без HTTH то удалить приклеть и проверить Сайт или в Поиск
-        url = url.toLowerCase()
-        if (url.contains("www."))       if (pingHttp(url.replace("www.", ""))) return true
-        if (url.startsWith("www."))     if (pingHttp("http://$url")) return true
-        if (url.startsWith("www."))     if (pingHttp("https://$url")) return true
-        if (url.startsWith("www."))     if (pingHttp(url.replace("www.", "http://"))) return true
-        if (url.startsWith("www."))     if (pingHttp(url.replace("www.", "https://"))) return true
-        if (!url.startsWith("http"))    if (pingHttp("http://$url")) return true
-        if (!url.startsWith("http"))    if (pingHttp("https://$url")) return true
+    fun listTryHttp(url: String): java.util.ArrayList<String> {
+        var url = url
+
+        val Zapros = java.util.ArrayList<String>()                      // 2 Лист сайтов
+
+        // Если точно не САЙТ - в ПОИСК (false)
+        if (!url.contains(".")) return Zapros
+        url = url.trim { it <= ' ' }.toLowerCase()
+        if (url.startsWith(".")) return Zapros
+        if (url.endsWith(".")) return Zapros
+        if (url.contains(" ")) return Zapros
+
         // Если это все таки URL - проверка на правильность и http <--> https
-        try { URL(url) } catch (e: MalformedURLException) { return false }
-        // Это наконец есть ли указанный "точно" сайт в Сети
-        if (pingHttp(url)) return true  // Простая прямая проверка на то что ввели в url
-        if (url.startsWith("http:"))    if (pingHttp(url.replace("http://", "https://"))) return true
-        if (url.startsWith("https:"))   if (pingHttp(url.replace("https://", "http://"))) return true
+        try {
+            URL(url)
+            // Это наконец есть ли указанный "точно" сайт в Сети
+            Zapros.add(url)  // Простая прямая проверка на то что ввели в url
+            if (url.startsWith("http:")) Zapros.add(url.replace("http://", "https://"))
+            if (url.startsWith("https:")) Zapros.add(url.replace("https://", "http://"))
+        } catch (e: MalformedURLException) {
+            Toast.makeText(applicationContext, "Запрос не является URL: $url", Toast.LENGTH_SHORT).show()
+        }
 
-        return false
+        // Если сайт без HTTH то приклеть и проверить Сайт или в Поиск
+        if (!url.startsWith("http")) Zapros.add("https://$url")
+        if (!url.startsWith("http")) Zapros.add("http://$url")
+
+        // Если WWW или м.б. сайт без HTTH то удалить приклеть и проверить Сайт или в Поиск
+        if (url.contains("www.")) Zapros.add(url.replace("www.", ""))
+        if (url.startsWith("www.")) Zapros.add("http://$url")
+        if (url.startsWith("www.")) Zapros.add("https://$url")
+        if (url.startsWith("www.")) Zapros.add(url.replace("www.", "http://"))
+        if (url.startsWith("www.")) Zapros.add(url.replace("www.", "http://"))
+
+        // TimeUnit.SECONDS.sleep(1);
+        return Zapros
     }
 
-    fun pingHttp(url: String): Boolean {
-        val httpConnection: HttpURLConnection
-        try {
-            httpConnection = URL(url).openConnection() as HttpURLConnection
-            httpConnection.requestMethod = "HEAD"
-            if (httpConnection.responseCode == 200) {
-                editText.setText(url)
-              //  Toast.makeText(applicationContext, "Сайт доступен  $url", Toast.LENGTH_LONG).show()
-                return true
+    fun tryHttp1(Zapros: java.util.ArrayList<String>): Boolean {
+        Toast.makeText(applicationContext, "Стартуют малые проверки задач=" + Zapros.size, Toast.LENGTH_LONG).show()
+        // Порождает кучу задач по количеству в листе
+        var answer: Boolean = false
+        var pingHttpTask: PingHttpTask
+        val listpingHttpTask = java.util.ArrayList<PingHttpTask>()    //  лист задач
+        for (Str in Zapros) {
+            pingHttpTask = PingHttpTask()
+            listpingHttpTask.add(pingHttpTask)
+            pingHttpTask.execute(Str)
+        }
+        // Проверяет кто из них жив true и  прибивает
+        for (task in listpingHttpTask) {
+            try {
+                if (task.get(3, TimeUnit.SECONDS)) answer = true
+                //if (task.get()) answer = true
+            } catch (e: TimeoutException) {
+            } catch (e: ExecutionException) {
+            } catch (e: InterruptedException) {
             }
-        } catch (e: IOException) {  }
-        // Toast.makeText(applicationContext, "САЙТ $url НЕ доступен", Toast.LENGTH_SHORT).show()
-        return false
+
+            task.cancel(true)
+        }
+        return answer
     }
-    fun Poisk(searchQuery: String): Boolean {
+
+    fun tryHttp2(Zapros: java.util.ArrayList<String>): Boolean {
+        var answer = false
+        Toast.makeText(applicationContext, "Стартует большая проверка шагов= " + Zapros.size, Toast.LENGTH_LONG).show()
+        pingHttpTask = PingHttpTask()
+        pingHttpTask!!.execute(*Zapros.toTypedArray())
         try {
-            val list = cs.cse().list(searchQuery).setCx(myCX)   //Set search parameter
-            val result = list.execute()    //Execute search
+            try {
+                answer = pingHttpTask!!.get(5, TimeUnit.SECONDS)
+            } catch (e: TimeoutException) {
+                pingHttpTask!!.cancel(true)
+            }
+
+        } catch (e: ExecutionException) {
+        } catch (e: InterruptedException) {
+        }
+
+        pingHttpTask!!.cancel(true)
+        return answer
+    }
+
+    inner class PingHttpTask : AsyncTask<String, Void, Boolean>() {
+        lateinit internal var httpConnection: HttpURLConnection
+        //private URL url;
+        private var temp: String = ""
+
+        override fun onPreExecute() {}
+
+        override fun doInBackground(vararg urls: String): Boolean {
+            for (url in urls) {
+                try {
+                    httpConnection = URL(url).openConnection() as HttpURLConnection
+                    httpConnection.requestMethod = "HEAD"
+                    // httpConnection.setReadTimeout(10000);
+                    val code = httpConnection.responseCode
+                    if (code == HttpURLConnection.HTTP_OK) { // HttpURLConnection.HTTP_OK (200) HttpURLConnection.HTTP_NOT_FOUND
+                        temp = url
+                        httpEdit = temp
+                        try {
+                        httpConnection.disconnect()
+                        } catch (e: Exception) {}
+                        return true
+                    }
+                } catch (e: MalformedURLException) {
+                } catch (e: IOException) {
+                }
+
+            }
+            temp = "catch $httpEdit"
+            return false
+        }
+
+        override fun onPostExecute(result: Boolean) {  // Почему-то сюда не попадаю
+            if (result) httpEdit = temp
+            try {
+                httpConnection.disconnect()
+            } catch (e: Exception) {
+            }
+
+            Toast.makeText(applicationContext, "$result  Подзадача  $temp", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private inner class MyWebViewClient : WebViewClient() {
+        @TargetApi(Build.VERSION_CODES.N)
+        override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
+            view.loadUrl(request.url.toString())
+            return true
+        }
+    }
+    inner class  Poisk :  AsyncTask<String, Void, Search?>() {
+        override fun doInBackground(vararg searchQuery: String): Search? {
+            var result: Search? = null
+            try {
+                val list = cs.cse().list(searchQuery[0]).setCx(myCX)   //Set search parameter
+                result = list.execute()    //Execute search
+                return result;
+            } catch (e: Exception) {
+                Log.d("POISK", "Exception")
+                Toast.makeText(applicationContext, "POISK : Exception", Toast.LENGTH_SHORT).show()
+            }
+            return result
+        }
+    }
+    fun LV(result:Search?): String? {
+
             if ((result != null) && (result.items != null)) {   // Вывод результата
                 val rezList = ArrayList<String>(result.items.size)
                 result.items.forEach { ri -> rezList.add(ri.title) }
@@ -123,19 +257,19 @@ class MainActivity : AppCompatActivity() {
                     // по позиции получаем выбранный элемент
                         val selectedItem = rezList[position]
                     Toast.makeText(applicationContext, "выбран $selectedItem"  , Toast.LENGTH_SHORT).show()
-                    Toast.makeText(applicationContext, "выбран \n  ${result.items[position].link}"  , Toast.LENGTH_LONG).show()
-                    startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(result.items[position].link)))
+                    Toast.makeText(applicationContext, "LINK \n  ${result.items[position].link}"  , Toast.LENGTH_LONG).show()
+                   // startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(result.items[position].link)))
+                  val LINK = result.items[position].link
+                    startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(LINK)))
+                   //httpEdit = LINK
                     }
-                return true
+
             } else {
                 Log.d("AS1", "resultgetItems()= null")
                 Toast.makeText(applicationContext, "resultgetItems()= null", Toast.LENGTH_SHORT).show()
             }
-        } catch (e: Exception) {
-            Log.d("POISK", "Exception")
-            Toast.makeText(applicationContext, "POISK : Exception", Toast.LENGTH_SHORT).show()
-        }
-        return false
+
+        return null
     }
 
 }
